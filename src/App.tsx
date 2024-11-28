836805e0b3b53fd934aaec25671cc5e472380517
Joseph Orji <joseph_orji@zenbyzenith.com>
2024-11-28 13:10:43 +0100

    feature: Travel itinerary platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CardComponent from "./components/Card/Card";
+import NavBar from "./components/NavBar/NavBar";
+import Sidebar from "./components/SideBar/Sidebar"; // Assuming you have Sidebar component created
+import TripItinerariesCard from "./components/TripItinaries/TripItinariesCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col h-full bg-gray-100">
+      <NavBar />
+      <div className="flex flex-1 mt-5 flex-col lg:flex-row">
+        <div className="lg:block lg:w-1/4 p-5 hidden md:block">
+          <Sidebar />
+        </div>
+        <div className="flex-1 p-5">
+          <CardComponent />
+          <TripItinerariesCard />
+        </div>
+      </div>
     </div>
   );
 }
